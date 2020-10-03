7bec902cfa1fbacbaf967d617844be73dd855b4a
Gerardo <Villalobos>
2020-10-03 11:08:12 -0500

    Commit inicial

---
+++ XamarinOrders/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinOrders.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public int EmployeeID { get; set; }
+        public double Freight { get; set; }
+        public string ShipCity { get; set; }
+        public bool Verified { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShipName { get; set; }
+        public string ShipCountry { get; set; }
+        public DateTime ShippedDate { get; set; }
+        public string ShipAddress { get; set; }
+    }
+}
---
+++ XamarinOrders/Services/WebAPIService.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using XamarinOrders.Models;
+
+namespace XamarinOrders.Services
+{
+    /// <summary>
+    /// Implementation of WebAPI service to be displayed.
+    /// </summary>
+    public class WebAPIService
+    {
+        #region Fields 
+
+        System.Net.Http.HttpClient client;
+
+        #endregion
+
+        #region Properties 
+
+        public ObservableCollection<Order> Items
+        {
+            get; private set;
+        }
+
+        public string WebAPIUrl
+        {
+            get; private set;
+        }
+
+        #endregion
+
+        #region Constructor
+        public WebAPIService()
+        {
+            client = new System.Net.Http.HttpClient();
+        }
+
+        #endregion
+
+        #region Methods
+        public async System.Threading.Tasks.Task<ObservableCollection<Order>> RefreshDataAsync()
+        {
+            //WebAPIUrl = "https://mtwdm-covid19.azurewebsites.net/api/order"; // Set your REST API url here
+
+            //Con esta Api de ejemplo se muestra
+            WebAPIUrl = "https://ej2services.syncfusion.com/production/web-services/api/Orders"; // Set your REST API url here
+            var uri = new Uri(WebAPIUrl);
+            try
+            {
+                var response = await client.GetAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Items = JsonConvert.DeserializeObject<ObservableCollection<Order>>(content);
+                    return Items;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
